b8e1175a080290060534436f594ee97d8ecffe27
Boaz Kaufman <bk1nyc@gmail.com>
2026-03-14 18:26:21 -0400

    Add Pets tests

--- src/app/Pets/Pets.test.tsx
+++ src/app/Pets/Pets.test.tsx
@@ -1,5 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import { beforeAll, beforeEach, describe, expect, it, vi } from 'vitest';
+import { cleanup, render, screen } from '@testing-library/react';
+import {
+	afterAll,
+	beforeAll,
+	beforeEach,
+	describe,
+	expect,
+	it,
+	vi,
+} from 'vitest';
 import '@testing-library/jest-dom/vitest';
 import { MemoryRouter } from 'react-router';
 import Pets from './Pets';
@@ -46,15 +54,78 @@ describe('Pets dashboard page with data', () => {
 			</MemoryRouter>,
 		);
 	});
-	beforeEach(() => {});
+
+	afterAll(cleanup);
 
 	it('renders a title', () => {
 		const title = screen.getByRole('heading', { name: /view all pets/i });
 		expect(title).toBeInTheDocument();
 	});
 
-	it('Displays a list of pet cards', async () => {
+	it('displays a list of pet cards', async () => {
 		const card = await screen.findByText(RegExp(TEST_PET_NAME, 'i'));
 		expect(card).toBeInTheDocument();
+	});
+});
+
+describe('Pets dashboard with no data', () => {
+	beforeAll(() => {
+		mockGetAccessTokenSilently.mockResolvedValue('fake-token');
+		mockPetsRead.mockResolvedValue({});
+
+		render(
+			<MemoryRouter>
+				<Pets />
+			</MemoryRouter>,
+		);
+	});
+	afterAll(cleanup);
+
+	it('says it is loading before data is loaded', () => {
+		const notif = screen.getByText(/loading/i);
+		expect(notif).toBeInTheDocument();
+	});
+
+	it('states that there were no pets found', async () => {
+		const notif = await screen.findByRole('heading', {
+			name: /no pets found/i,
+		});
+		expect(notif).toBeInTheDocument();
+	});
+
+	it('gives a link to the add pets page', async () => {
+		const notif = await screen.findByRole('link', {
+			name: /adopt one/i,
+		});
+		expect(notif).toBeInTheDocument();
+	});
+});
+
+describe('Pets dashboard with errors', () => {
+	it('displays a backend read error', async () => {
+		mockGetAccessTokenSilently.mockResolvedValue('fake-token');
+		mockPetsRead.mockRejectedValue(new Error('Something went wrong'));
+		render(
+			<MemoryRouter>
+				<Pets />
+			</MemoryRouter>,
+		);
+		const err = await screen.findByText(/there was a problem/i);
+		expect(err).toBeInTheDocument();
+		cleanup();
+	});
+	it('displays an auth0 error', async () => {
+		mockGetAccessTokenSilently.mockRejectedValue(
+			new Error('SomethingWentWrong)'),
+		);
+		mockPetsRead.mockResolvedValue({});
+		render(
+			<MemoryRouter>
+				<Pets />
+			</MemoryRouter>,
+		);
+		const err = await screen.findByText(/there was a problem/i);
+		expect(err).toBeInTheDocument();
+		cleanup();
 	});
 });
