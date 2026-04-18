580fccc77a6429cb7bb68b8b6939075d32a732a8
Boaz Kaufman <bk1nyc@gmail.com>
2026-04-18 13:39:16 -0400

    Reformat disown/adopt button

--- src/app/components/PetCard/DisownAdoptButton.tsx
+++ src/app/components/PetCard/DisownAdoptButton.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { petAdopt, petDisown } from '../../utils/networkutils';
 
 interface DisownAdoptButtonProps {
@@ -9,26 +10,35 @@ interface DisownAdoptButtonProps {
 
 export default function DisownAdoptButton(props: DisownAdoptButtonProps) {
 	const { token, id, fetchPets, disownMode } = props;
+	const [notif, setNotif] = useState<string | undefined>();
 	const disownPet = () => {
 		petDisown(token, id).then(() => {
 			fetchPets(token);
-		});
+		})
+			.then(() => setNotif('Pet sent to the pound.'))
+			.catch((error) => setNotif(`Error: ${error}`));
 	};
 	const adoptPet = () => {
 		petAdopt(token, id).then(() => {
 			fetchPets(token);
-		});
+		})
+			.then(() => setNotif('Pet adopted!'))
+			.catch((error) => setNotif(`Error: ${error}`));;
 	};
-	if (disownMode)
-		return (
-			<button type="button" onClick={disownPet}>
-				Disown
-			</button>
-		);
-	else
-		return (
-			<button type="button" onClick={adoptPet}>
-				Adopt
-			</button>
-		);
+
+	return (
+		<>
+			{
+				disownMode ?
+					<button type="button" onClick={disownPet}>
+						Disown
+					</button>
+
+					: <button type="button" onClick={adoptPet}>
+						Adopt
+					</button>
+			}
+			{notif && <p>{notif}</p>}
+		</>
+	);
 }
